30f07bb16c5f0158075bae5cc0bd0b942f5a67cf
Jimmy Bush <jebush94@hotmail.com>
2016-08-04 13:49:30 -0400

    Progress update at 150pm

--- src/Duck.java
+++ src/Duck.java
@@ -2,4 +2,25 @@
  * Created by rush on 8/4/16.
  */
 public class Duck {
+    String duckName;
+    String duckType;
+    int age;
+    int numberFeather;
+    boolean makesItHome;
+
+    public void homeBound(boolean homes){
+        if (homes == true){
+        makesItHome = true;
+
+        System.out.println("The duck will go home");}
+        else {
+            System.out.println("The duck will go home with a Hunter");
+        }
+    }
+
+    public String toString(){
+
+        return "";
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,17 @@
  * Created by rush on 8/4/16.
  */
 public class Main {
+    public static void main(String[] args) {
+        //Calls to the Duck class and creates two new instances
+        Duck myDuck = new Duck();
+        Duck myDuck2 = new Duck();
+
+
+        int age = myDuck.age;
+        System.out.println("Duck age is " + age);
+
+        //Calling a method from duck and displays the ducks fate
+        myDuck.homeBound(false);
+    }
 }
+
