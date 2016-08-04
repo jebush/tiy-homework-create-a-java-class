5da5fe6ec2d69ce431611684a3ab55de6d9db1fc
Jimmy Bush <jebush94@hotmail.com>
2016-08-04 14:36:10 -0400

    This is almost done

--- src/Duck.java
+++ src/Duck.java
@@ -8,19 +8,48 @@ public class Duck {
     int numberFeather;
     boolean makesItHome;
 
+    public void typing(){
+        duckType = "Dabbling";
+        System.out.println(duckName + " is a " + duckType + " duck.");
+    }
+
+    public void featherFinder(int age){
+        if (age <= 1){
+            int numberFeather = 200;
+            System.out.println("The number of feathers that " + duckName + " has is " + numberFeather);
+        }
+        else if (age <= 2){
+            int numberFeather = 340;
+            System.out.println("The number of feathers that " + duckName + " has is " + numberFeather);
+        }
+        else if (age <= 4){
+            int numberFeather = 540;
+            System.out.println("The number of feathers that " + duckName + " has is " + numberFeather);
+        }
+        else if (age <= 6){
+            int numberFeather = 640;
+            System.out.println("The number of feathers that " + duckName + " has is " + numberFeather);
+        }
+        else {
+            int numberFeather = 840;
+            System.out.println("The number of feathers that " + duckName + " has is " + numberFeather);
+        }
+
+    }
+
     public void homeBound(boolean homes){
         if (homes == true){
         makesItHome = true;
 
-        System.out.println("The duck will go home");}
+        System.out.println(duckName + " will go home to Canada!");}
         else {
-            System.out.println("The duck will go home with a Hunter");
+            System.out.println(duckName + " will go home with a Hunter as a main dinner dish!");
         }
     }
 
     public String toString(){
 
-        return "";
+        return duckName + " was banded so this is a rare find.";
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,46 @@ public class Main {
         Duck myDuck = new Duck();
         Duck myDuck2 = new Duck();
 
+        //These are creating a myDuck variables
+        myDuck.duckName = "Woodie";
+        myDuck.age = 6;
 
-        int age = myDuck.age;
-        System.out.println("Duck age is " + age);
+        //This method print out the type of duck it is
+        myDuck.typing();
+
+        //Prints out the statement
+        System.out.println(myDuck.duckName + "'s age is " + myDuck.age + " years old.");
+
+        //This has an output of banded status
+        System.out.println(myDuck.toString());
+
+        //Determines the number of feathers based on age from the method featherFinder
+        myDuck.featherFinder(myDuck.age);
 
         //Calling a method from duck and displays the ducks fate
         myDuck.homeBound(false);
+
+        //
+        //These provide a break for readability between each duck
+        System.out.println();
+        System.out.println();
+
+        //These are creating a myDuck variables
+        myDuck2.duckName = "Mallard";
+        myDuck2.age = 2;
+        myDuck2.makesItHome = false;
+
+        //This method print out the type of duck it is
+        myDuck2.typing();
+
+        //Prints out the statement
+        System.out.println(myDuck2.duckName + "'s age is " + myDuck2.age + " years old.");
+
+        //Determines the number of feathers based on age from the method featherFinder
+        myDuck2.featherFinder(myDuck2.age);
+
+        //Calling a method from duck and displays the ducks fate
+        myDuck2.homeBound(true);
     }
 }
 
